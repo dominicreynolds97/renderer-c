8bc009d463bcdc2cc2f620c60585ccd9ef429a9b
dominicreynolds97 <dominicrjreynolds@gmail.com>
2026-02-25 22:58:00 +0000

    2D or not 2D

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <SDL.h>
 #include <stdint.h>
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #define WIDTH 1080
 #define HEIGHT 720
@@ -21,6 +23,48 @@ typedef struct {
   int x;
   int y;
 } Vec2i;
+
+typedef struct {
+  float x;
+  float y;
+} Vec2f;
+
+typedef struct {
+  float x;
+  float y;
+  float z;
+} Vec3f;
+
+Vec3f vec3f_sub(Vec3f a, Vec3f b) {
+  return (Vec3f){a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+Vec3f vec3f_cross(Vec3f a, Vec3f b) {
+  return (Vec3f) {
+    a.y * b.z - a.z * b.y,
+    a.z * b.x - a.x * b.z,
+    a.x * b.y - a.y * b.x
+  };
+}
+
+float vec3f_dot(Vec3f a, Vec3f b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+float vec3f_length(Vec3f v) {
+  return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+Vec3f vec3f_normalize(Vec3f v) {
+  float len = vec3f_length(v);
+  return (Vec3f){v.x / len, v.y / len, v.z / len};
+}
+
+float compute_light(Vec3f normal, Vec3f light_dir) {
+  float intensity = vec3f_dot(normal, light_dir);
+  if (intensity < 0) intensity = 0;
+  return intensity;
+}
 
 typedef struct {
   float wa;
@@ -236,8 +280,12 @@ void draw_triangle_filled(Pixel *fb, Vec2i a, Vec2i b, Vec2i c, Pixel color) {
   }
 }
 
-void draw_triangle_colored(Pixel *fb, Vec2i a, Vec2i b, Vec2i c, Color ca, Color cb, Color cc) {
+typedef struct {
+  Vec2i max;
+  Vec2i min;
+} Bounds;
 
+Bounds get_triangle_bounds(Vec2i a, Vec2i b, Vec2i c) {
   int min_x = a.x < b.x
     ? (a.x < c.x ? a.x : c.x)
     : (b.x < c.x ? b.x : c.x );
@@ -251,8 +299,14 @@ void draw_triangle_colored(Pixel *fb, Vec2i a, Vec2i b, Vec2i c, Color ca, Color
     ? (a.y > c.y ? a.y : c.y)
     : (b.y > c.y ? b.y : c.y );
 
-  for (int y = min_y; y <= max_y; y++) {
-    for (int x = min_x; x <= max_x; x++) {
+  return (Bounds){(Vec2i){max_x, max_y},(Vec2i){min_x, min_y}};
+}
+
+void draw_triangle_colored(Pixel *fb, Vec2i a, Vec2i b, Vec2i c, Color ca, Color cb, Color cc) {
+  Bounds bounds = get_triangle_bounds(a, b, c);
+
+  for (int y = bounds.min.y; y <= bounds.max.y; y++) {
+    for (int x = bounds.min.x; x <= bounds.max.x; x++) {
       Barycentric bary = barycentric(a, b, c, (Vec2i){x,y});
       if (!barycentric_inside(bary)) continue;
 
@@ -263,6 +317,98 @@ void draw_triangle_colored(Pixel *fb, Vec2i a, Vec2i b, Vec2i c, Color ca, Color
       put_pixel(fb, x, y, rgb(r, g, b));
     }
   }
+}
+
+typedef struct {
+  uint8_t *data;
+  int width;
+  int height;
+  int channels;
+} Texture;
+
+Texture load_texture(const char *path) {
+  Texture t;
+  t.data = stbi_load(path, &t.width, &t.height, &t.channels, 4);
+  if (!t.data) {
+    printf("Failed to load texture: %s\n", path);
+  }
+  return t;
+}
+
+void free_texture(Texture *t) {
+  stbi_image_free(t->data);
+}
+
+Pixel sample_texture(Texture *t, float u, float v) {
+  if (v < 0) u = 0; if (u > 1) u = 1;
+  if (v < 0) v = 0; if (v > 1) v = 1;
+
+  int x = (int)(u * (t->width - 1));
+  int y = (int)(v * (t->height - 1));
+
+  int idx = (y * t->width + x) * 4;
+  return rgb(t->data[idx], t->data[idx +1], t->data[idx + 2]);
+}
+
+void draw_triangle_shaded(Pixel *fb, Vec2i a, Vec2i b, Vec2i c,
+    Color base_color, Vec3f light_dir) {
+  Vec3f normal = {0.0f, 0.0f, 1.0f};
+  float intensity = compute_light(normal, light_dir);
+
+  Color shaded = {
+    (uint8_t)(base_color.r * intensity),
+    (uint8_t)(base_color.g * intensity),
+    (uint8_t)(base_color.b * intensity)
+  };
+
+  draw_triangle_colored(fb, a, b, c, shaded, shaded, shaded);
+}
+
+void draw_triangle_textured(Pixel *fb, Vec2i a, Vec2i b, Vec2i c,
+    Vec2f uva, Vec2f uvb, Vec2f uvc,
+    Texture *tex) {
+  Bounds bounds = get_triangle_bounds(a, b, c);
+
+  for (int y = bounds.min.y; y <= bounds.max.y; y++) {
+    for (int x = bounds.min.x; x <= bounds.max.x; x++) {
+      Barycentric bary = barycentric(a, b, c, (Vec2i){x,y});
+      if (!barycentric_inside(bary)) continue;
+
+      float u = bary.wa * uva.x + bary.wb * uvb.x + bary.wc * uvc.x;
+      float v = bary.wa * uva.y + bary.wb * uvb.y + bary.wc * uvc.y;
+
+      put_pixel(fb, x, y, sample_texture(tex, u, v));
+    }
+  }
+}
+
+void draw_triangle_textured_perspective(Pixel *fb, Vec2i a, Vec2i b, Vec2i c,
+    Vec2f uva, Vec2f uvb, Vec2f uvc,
+    float wa, float wb, float wc,
+    Texture *tex) {
+  Bounds bounds = get_triangle_bounds(a, b, c);
+  for (int y = bounds.min.y; y <= bounds.max.y; y++) {
+    for (int x = bounds.min.x; x <= bounds.max.x; x++) {
+      Barycentric bary = barycentric(a, b, c, (Vec2i){x,y});
+      if (!barycentric_inside(bary)) continue;
+
+      float one_over_w = bary.wa / wa + bary.wb / wb + bary.wc / wc;
+
+
+      float u = (bary.wa * uva.x + bary.wb * uvb.x + bary.wc * uvc.x) / one_over_w;
+      float v = (bary.wa * uva.y + bary.wb * uvb.y + bary.wc * uvc.y) / one_over_w;
+
+      put_pixel(fb, x, y, sample_texture(tex, u, v));
+    }
+  }
+}
+
+void draw_quad_textured(Pixel *fb,
+    Vec2i a, Vec2i b, Vec2i c, Vec2i d,
+    Vec2f uva, Vec2f uvb, Vec2f uvc, Vec2f uvd,
+    Texture *tex) {
+  draw_triangle_textured(fb, a, b, c, uva, uvb, uvc, tex);
+  draw_triangle_textured(fb, b, d, c, uvb, uvd, uvc, tex);
 }
 
 void clear(Pixel *fb, Pixel color) {
@@ -299,6 +445,7 @@ int main(void) {
   );
 
   Pixel framebuffer[WIDTH * HEIGHT];
+  Texture tex = load_texture("Sprite-0001.png");
 
   int running = 1;
   while (running) {
@@ -371,10 +518,45 @@ int main(void) {
       (Color){0,   0,   255}
     );
 
+
+    draw_triangle_textured(framebuffer,
+      (Vec2i){640, 100},
+      (Vec2i){1100, 600},
+      (Vec2i){200, 600},
+      (Vec2f){0.5f, 0.0f},
+      (Vec2f){1.0f, 1.0f},
+      (Vec2f){0.0f, 1.0f},
+      &tex
+    );
+
+    draw_quad_textured(framebuffer,
+      (Vec2i){200, 100}, (Vec2i){800, 100},
+      (Vec2i){100, 400}, (Vec2i){800, 500},
+      (Vec2f){0.0f, 0.0f}, (Vec2f){1.0f, 0.0f},
+      (Vec2f){0.0f, 1.0f}, (Vec2f){1.0f, 1.0f},
+      &tex
+    );
+
+    Vec3f light = vec3f_normalize((Vec3f){-1.0f, 1.0f, 1.0f}); // light from top-right
+    draw_triangle_shaded(framebuffer,
+      (Vec2i){640, 100}, (Vec2i){900, 600}, (Vec2i){380, 600},
+      (Color){255, 100, 50},
+      light
+    );
+
+    draw_triangle_textured_perspective(framebuffer,
+      (Vec2i){200, 100}, (Vec2i){800, 100}, (Vec2i){500, 600},
+      (Vec2f){0.0f, 0.0f}, (Vec2f){1.0f, 0.0f}, (Vec2f){0.5f, 1.0f},
+      1.0f, 1.0f, 1.0f,
+      &tex
+    );
+
     SDL_UpdateTexture(texture, NULL, framebuffer, WIDTH * sizeof(Pixel));
     SDL_RenderCopy(sdl_renderer, texture, NULL, NULL);
     SDL_RenderPresent(sdl_renderer);
   }
+
+  free_texture(&tex);
 
   SDL_DestroyTexture(texture);
   SDL_DestroyRenderer(sdl_renderer);
